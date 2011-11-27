b6be5d50ac8a4815efa4904e864ef6cc6e2db8e4
Tim Jarrett <tjarrett@Tims-Core-2-Unibody-MBP.local>
2011-11-27 10:53:26 -0500

    Changing the constant string value to something more useful

--- src/jarrett/tim/rg/Emit.java
+++ src/jarrett/tim/rg/Emit.java
@@ -7,39 +7,39 @@ package jarrett.tim.rg;
  */
 public class Emit
 {
-    static public final String HEAT_UP = "Heat[UP]";
+    static public final String HEAT_UP = "Heat|UP";
     
-    static public final String CLAP_ALL = "Clap[ALL]";
+    static public final String CLAP_ALL = "Clap|ALL";
     
-    static public final String PULL_LEFT = "Pull[LEFT]";
+    static public final String PULL_LEFT = "Pull|LEFT";
     
-    static public final String PULL_RIGHT = "Pull[RIGHT]";
+    static public final String PULL_RIGHT = "Pull|RIGHT";
     
-    static public final String PULL_UP = "Pull[UP]";
+    static public final String PULL_UP = "Pull|UP";
     
-    static public final String PULL_DOWN = "Pull[DOWN]";
+    static public final String PULL_DOWN = "Pull|DOWN";
     
-    static public final String ELECTRIC_ON_ALL = "ElectricOn[ALL]";
+    static public final String ELECTRIC_ON_ALL = "ElectricOn|ALL";
     
-    static public final String ELECTRIC_ON_RIGHT = "ElectricOn[RIGHT]";
+    static public final String ELECTRIC_ON_RIGHT = "ElectricOn|RIGHT";
     
-    static public final String ELECTRIC_ON_DOWN = "ElectricOn[DOWN]";
+    static public final String ELECTRIC_ON_DOWN = "ElectricOn|DOWN";
     
-    static public final String ELECTRIC_OFF_ALL = "ElectricOff[ALL]";
+    static public final String ELECTRIC_OFF_ALL = "ElectricOff|ALL";
     
-    static public final String ELECTRIC_OFF_RIGHT = "ElectricOff[RIGHT]";
+    static public final String ELECTRIC_OFF_RIGHT = "ElectricOff|RIGHT";
     
-    static public final String ELECTRIC_OFF_DOWN = "ElectricOff[DOWN]";
+    static public final String ELECTRIC_OFF_DOWN = "ElectricOff|DOWN";
     
-    static public final String STEAM_RIGHT = "Steam[RIGHT]";
+    static public final String STEAM_RIGHT = "Steam|RIGHT";
     
-    static public final String TURN_ALL = "Turn[ALL]";
+    static public final String TURN_ALL = "Turn|ALL";
     
-    static public final String RELEASE_BOTH = "Release[BOTH]";
+    static public final String RELEASE_BOTH = "Release|BOTH";
     
-    static public final String ALEX_ALL = "Alex[ALL]";
+    static public final String ALEX_ALL = "Alex|ALL";
     
-    static public final String WATER_DOWN = "Water[DOWN]";
+    static public final String WATER_DOWN = "Water|DOWN";
     
     
     
